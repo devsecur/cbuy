9568dc79a30188b8d07d9f306d0c84de1bcafe0e
Fru Boris <fruboris24@gmail.com>
2023-10-12 16:32:20 +0100

    feat:first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+      <>
+        <div className="bg-blue-800 mx-auto my-[50vh] text-3xl text-red-900 font-bold underline">
+          Hello Word d ed d ee
+        </div>
+        
+      </>
+    );
+  };
+  export default Home;
+  
